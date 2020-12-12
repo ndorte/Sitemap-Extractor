b83f2e249460145908ecabc90428ad68f58cc9bb
Uğur Kubilay ÇAM <32200607+ndorte@users.noreply.github.com>
2020-12-12 16:16:50 +0300

    Create sitemap-extractor.py

---
+++ sitemap-extractor.py
@@ -0,0 +1,19 @@
+#Save your xml file to txt
+
+filename = "sitemap1.txt"
+
+with open(filename, "r", encoding="utf-8") as sitemap:
+    for url in sitemap.readlines():
+        if "<loc>" in url:
+            urlstrip = url.strip()
+
+            # if you want include domain use this line
+            print (urlstrip[5:-6])
+
+            # if you want exclude domain use this line
+            #domain = "https://example.com"
+            #rep = urlstrip.replace(domain,"")
+            #print(rep[5:-6])
+
+        else:
+            continue
